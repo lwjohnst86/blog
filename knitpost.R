6f7db44730c816d4e0d22d17e0e8b68a34f7bf30
Luke W Johnston <lwjohnst@gmail.com>
2016-08-05 14:37:39 -0400

    Changed build from makefile to knitr

--- knitpost.R
+++ knitpost.R
@@ -1,31 +1,31 @@
 #!/usr/bin/Rscript --vanilla
 
-# Taken and modified from
-# http://varianceexplained.org/pages/workflow.html
-
-# compiles all .Rmd files in _R directory into .md files in the _posts
+# compiles all .Rmd files in _drafts directory into .md files in the _posts
 # directory, if the input file is older than the output file.
+# run ./knitpost.R to update all knitr files that need to be updated.
 
-# run ./knitpages.R to update all knitr files that need to be updated.
-
-KnitPost <- function(input, outfile, base.url = '/') {
+KnitPost <- function(input) {
     # this function is a modified version of an example here:
     # http://jfisher-usgs.github.com/r/2012/07/03/knitr-jekyll/
-    require(knitr);
-    opts_knit$set(base.url = base.url)
+    outfile <- rename_to_post(input)
+    knitr::opts_knit$set(base.url = '{{ site.github.url }}/')
     fig.path <- paste0("images/", sub(".Rmd$", "", basename(input)), "/")
-    opts_chunk$set(fig.path = fig.path)
-    render_jekyll()
-    knit(input, outfile, envir = parent.frame())
+    knitr::opts_chunk$set(fig.path = fig.path,
+                          comment = '#>')
+    knitr::render_jekyll()
+    knitr::knit(input, outfile, envir = parent.frame())
 }
 
-for (infile in list.files("_R", pattern="*.Rmd", full.names=TRUE)) {
-    outfile = paste0("_posts/", sub(".Rmd$", ".md", basename(infile)))
+if_newer <- function(draft, post)
+    !file.exists(post) | file.info(draft)$mtime > file.info(post)$mtime
 
-    # knit only if the input file is the last one modified
-    if (!file.exists(outfile) |
-        file.info(infile)$mtime > file.info(outfile)$mtime) {
-        KnitPost(infile, outfile)
-    }
+posts_to_update <- function(draft) {
+    post <- rename_to_post(draft)
+    draft[if_newer(draft = draft, post = post)]
 }
 
+rename_to_post <- function(draft)
+    paste0("_posts/", sub(".Rmd$", ".md", basename(draft)))
+
+drafts <- list.files("_drafts", pattern = "*.Rmd", full.names = TRUE)
+sapply(posts_to_update(drafts), KnitPost)
